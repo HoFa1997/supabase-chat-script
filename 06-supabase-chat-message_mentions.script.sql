d2fe9c864ad58a15022239cdf4b6684c429a1a7d
hossein <marzban98@gmail.com>
2023-11-29 19:01:41 +0330

    preview

--- 06-supabase-chat-message_mentions.script.sql
+++
@@ -1,7 +0,0 @@
-
-
-CREATE TABLE public.message_mentions (
-  id            UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
-  message_id    UUID REFERENCES public.messages(id) ON DELETE CASCADE,
-  mentioned_user_id UUID REFERENCES public.users(id) ON DELETE CASCADE
-);
--- 11-supabase-chat-other.script.sql
+++
@@ -1,9 +0,0 @@
--- Send "previous data" on change
-alter table public.users replica identity full;
-alter table public.channels replica identity full;
-alter table public.messages replica identity full;
-
--- add tables to the publication
-alter publication supabase_realtime add table public.users;
-alter publication supabase_realtime add table public.channels;
-alter publication supabase_realtime add table public.messages;
